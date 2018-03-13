6e04dafec20d8b4783c0c6f56c50cfdf76d0b17d
maheshsidharth098 <31186183+maheshsidharth098@users.noreply.github.com>
2018-03-13 19:42:08 +0530

    Add files via upload

---
+++ b'day.js
@@ -0,0 +1,58 @@
+
+
+
+           
+           window.sr = ScrollReveal();
+
+           sr.reveal('.btn', {
+               duration: 2000,
+               origin: 'top',
+               distance: '60px',
+           });
+
+           sr.reveal('#bio', {
+               duration: 2000,
+               origin: 'bottom',
+               distance: '100px',
+               reset: true
+           });
+           
+           sr.reveal('#testimonial', {
+               duration: 2000,
+               origin: 'bottom',
+               viewFactor: 0.3,
+               delay: 1000,
+               reset: true
+           });
+           
+           sr.reveal('#elsalvador', {
+               duration: 2000,
+               origin: 'bottom',
+               viewFactor: 0.5,
+               reset: true
+           });
+           
+           sr.reveal('#therose', {
+               duration: 2000,
+               origin: 'bottom',
+               viewFactor: 0.3,
+               reset: true
+           });
+           
+           sr.reveal('#thecouple', {
+               duration: 2000,
+               origin: 'bottom',
+               viewFactor: 0.3,
+               reset: true
+           });
+           
+           sr.reveal('#pao', {
+               duration: 2000,
+               origin: 'right',
+               distance: "1000px",
+               viewFactor: 0.2,
+               reset: true
+           });
+           
+       
+       
